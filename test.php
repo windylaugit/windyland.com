1b92cac36cb1cfc3efd8fae5b4eb8ea9f6af8277
AndyLau.Diansan <AndyLau@diansan.com>
2016-07-01 12:44:59 +0800

    2

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+*
+* @copyright(c) 2015
+* @author AndyLau <i@windyland.com>
+* @package 
+* @version V1.0.1
+* @date 2015-7-6
+*/
+namespace Top;
+class Test{
+	public $a = null;
+	public $b = null;
+}
+
+$t = new Test('aa','bb');
+
+var_dump($t);
